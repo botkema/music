817c15b8ac70afc96dfcacc43f3c1eb1065632da
botkema <aboali57587@gmail.com>
2024-09-24 10:50:52 +0300

    Update config.py

--- config.py
+++ config.py
@@ -33,7 +33,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/botbsam/music",
+    "https://github.com/botkema/music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
